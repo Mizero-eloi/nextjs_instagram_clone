4e18c1e10a7dd6eccb381be06abae3bb68003ce7
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-08-30 13:49:26 +0200

    navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { navbarIcons } from "../utils/constants";
+
+import Logo from "../utils/logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white px-4 py-3">
+      <div className="flex justify-between md:w-[70%] md:m-auto">
+        {/* logo */}
+        <Link href="/">
+          <div className="w-[100px] md:w-[130px] ">
+            <Image
+              className="cursor-pointer"
+              src={Logo}
+              alt="TikTik"
+              layout="responsive"
+            />
+          </div>
+        </Link>
+
+        {/* search */}
+        <div className="hidden md:block">search</div>
+
+        {/* icons */}
+        <div className="flex gap-7">
+          {navbarIcons.map((icon) => (
+            <Link key={icon.name} href={`/${icon.name}`}>
+              <button className="text-2xl hover:text-gray-600 flex justify-center items-center">
+                {icon.icon}
+              </button>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ utils/constants.tsx
@@ -0,0 +1,30 @@
+import { CgHome, CgAddR } from "react-icons/cg";
+import { RiMessengerLine } from "react-icons/ri";
+import { MdOutlineExplore } from "react-icons/md";
+import { BiHeart } from "react-icons/bi";
+import { AiOutlineLogin } from "react-icons/ai";
+import { IoMdAdd } from "react-icons/io";
+import { FiHome } from "react-icons/fi";
+
+export const navbarIcons = [
+  {
+    name: "Home",
+    icon: <FiHome />,
+  },
+  {
+    name: "Messenger",
+    icon: <RiMessengerLine />,
+  },
+  {
+    name: "upload",
+    icon: <CgAddR />,
+  },
+  {
+    name: "Explore",
+    icon: <BiHeart />,
+  },
+  {
+    name: "login",
+    icon: <AiOutlineLogin />,
+  },
+];
