5ac16e01c46e0d454bebc2aa37916ccb6b90befc
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-09-25 12:20:10 +0200

    corrected the login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { useGoogleLogin } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 
 import { createOrGetUser } from "../utils";
 import useAuthStore from "./../store/authStore";
+import { useRouter } from "next/router";
 
 const Login = () => {
   const { addUser } = useAuthStore();
+  const router = useRouter();
+  console.log({ router });
 
-  const theGoogleLogin = useGoogleLogin({
-    onSuccess: (response) => createOrGetUser(response, addUser),
-    onError: () => console.log("Error"),
-  });
+  //   console.log("google token", process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN);
+
+  //
 
   return (
-    <button
-      onClick={() => theGoogleLogin()}
-      className="px-[31px] relative left-1/2 top-[300px]  py-[15px] rounded text-white bg-blue-400"
-    >
-      Log in with google
-    </button>
+    <div className="w-[100px] relative top-[200px] m-auto ">
+      <GoogleLogin
+        onSuccess={(response) => {
+            router.push("/");
+            return createOrGetUser(response, addUser);
+        }}
+        onError={() => console.log("Error")}
+      />
+    </div>
   );
 };
 
