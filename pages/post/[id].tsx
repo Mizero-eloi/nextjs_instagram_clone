fd98e37a2f58aad0c99f2a302191fe187999243f
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-09-26 07:57:32 +0200

    corrected the server for production

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -20,7 +20,9 @@ export const getServerSideProps = async ({
 }: {
   params: { id: string };
 }) => {
-  const { data } = await axios.get(`http://localhost:3000/api/post/${id}`);
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/post/${id}`
+  );
 
   return {
     props: {
